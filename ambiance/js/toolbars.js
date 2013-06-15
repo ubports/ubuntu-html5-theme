d9fed6062d90966cf43231bdc8ebff81a1ce1c22
Adnane Belmadiaf <daker@ubuntu.com>
2013-06-15 14:41:41 +0100

    Added more listitems CSS Added Indexbar CSS Fixed Active state link for the popover

---
+++ ambiance/js/toolbars.js
@@ -0,0 +1,15 @@
+!function () {
+  var findToolbars = function (target) {
+    var i,
+        toolbars = document.querySelectorAll("footer");
+    for (; target && target !== document; target = target.parentNode) {
+      for (i = toolbars.length; i--;) { if (toolbars[i] === target) return target; }
+    }
+  };
+
+  window.addEventListener('touchend', function (event) {
+    var toolbar = findToolbars(event.target);
+    if (!toolbar) return;
+    if (toolbar) toolbar.classList.toggle('revealed');
+  });
+}();
