338fc312c705d6275216216979927c7727462496
Adnane Belmadiaf <daker@ubuntu.com>
2013-06-15 18:35:51 +0100

    More Tabs fixes (JS)

--- ambiance/js/tabs.js
+++ ambiance/js/tabs.js
@@ -1,41 +1,9 @@
-
-/*
-!function () {
-  var getTarget = function (target) {
-    var i, tabs = document.querySelectorAll('.tabs-inner li');
-    for (; target && target !== document; target = target.parentNode) {
-      for (i = tabs.length; i--;) { if (tabs[i] === target) return target; }
-    }
-  };
-
-  window.addEventListener('click', function (e) {
-    var activeTab;
-    var targetTab;
-    var className     = 'active';
-    var classSelector = '.' + className;
-    var targetAnchor  = getTarget(e.target);
-
-    if (!targetAnchor) return;
-
-    targetTab = targetAnchor;
-    activeTab = targetTab.parentNode.querySelector(classSelector);
-
-    // Highlight the target tab
-    if (activeTab) activeTab.classList.remove(className);
-    targetTab.classList.add(className);
-
-  });
-
-}();*/
-
-
-
 !function () {
 
   var tabs;
   var pageX;
   var pageY;
-  var tabNumber;
+  var isScrolling;
 
   var getTabs = function (target) {
     var tabs = document.querySelector('.tabs-inner ul');
@@ -53,12 +21,11 @@
     tabs = getTabs(e.target);
     if (!tabs) return;
 
-    isScrolling    = undefined;
-    tabsWidth    = tabs.offsetWidth;
-    resistance     = 1;
-    pageX          = e.touches[0].pageX;
-    pageY          = e.touches[0].pageY;
-    lastTab      = -(tabs.children.length - 1);
+    isScrolling = undefined;
+    tabsWidth   = tabs.offsetWidth;
+    resistance  = 1;
+    pageX       = e.touches[0].pageX;
+    pageY       = e.touches[0].pageY;
 
     tabs.style['-webkit-transition-duration'] = 0;
   };
@@ -80,18 +47,13 @@
     offsetX = (deltaX / resistance) + getScroll();
 
     e.preventDefault();
-    resistance = tabNumber == 0         && deltaX > 0 ? (pageX / tabsWidth) + 1.25 :
-                 tabNumber == lastTab && deltaX < 0 ? (Math.abs(pageX) / tabsWidth) + 1.25 : 1;
-
     tabs.style.webkitTransform = 'translate3d(' + offsetX + 'px,0,0)';
   };
-
 
   var onTouchEnd = function (e) {
     if (!tabs || isScrolling) return;
 
-    offsetX = tabNumber * tabsWidth;
-
+    offsetX = 0 * tabsWidth;
     tabs.style['-webkit-transition-duration'] = '.2s';
     tabs.style.webkitTransform = 'translate3d(' + offsetX + 'px,0,0)';
   };
