e7a5cb3987ff71743ca84868e0ff65bec8002717
Alexandre Abreu <alexandre.abreu@canonical.com>
2013-07-03 09:57:41 -0400

    Add copyright headers to source files

--- 0.1/ambiance/js/core.js
+++ 0.1/ambiance/js/core.js
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2013 Canonical Ltd.
+ *
+ * This file is part of ubuntu-html5-theme.
+ *
+ * ubuntu-html5-theme is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * webbrowser-app is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 var UbuntuUI = (function () {
 
     PAGESTACK_BACK_ID = 'ubuntu-pagestack-back';
