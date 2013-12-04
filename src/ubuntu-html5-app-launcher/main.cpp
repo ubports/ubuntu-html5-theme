d9b8a1325d250093d0700269e718e34654943c42
Alexandre Abreu <alexandre.abreu@canonical.com>
2013-12-04 16:01:20 -0500

    Add container binary launcher

---
+++ src/ubuntu-html5-app-launcher/main.cpp
@@ -0,0 +1,166 @@
+/*
+ * Copyright 2013 Canonical Ltd.
+ *
+ * This file is part of ubuntu-html5-theme.
+ *
+ * ubuntu-html5-theme is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * ubuntu-html5-theme is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "config.h"
+
+#include <QGuiApplication>
+#include <QtQuick/QQuickItem>
+#include <QtQuick/QQuickView>
+#include <QDebug>
+#include <QFileInfo>
+#include <QLibrary>
+#include <QQmlEngine>
+#include <QQmlContext>
+#include <QQmlComponent>
+
+
+void setUpQmlImportPathIfNecessary()
+{
+    QString importPath = Webapp::Config::getContainerImportPath();
+    if ( !importPath.isEmpty())
+    {
+        qDebug() << "Setting import path to: " << importPath;
+        qputenv("QML2_IMPORT_PATH", importPath.toLatin1());
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    if (!app.arguments().count())
+    {
+        qCritical() << "Invalid inputs args";
+        return EXIT_FAILURE;
+    }
+    const QString WWW_LOCATION_ARG_HEADER = "--www=";
+    const QString MAXIMIZED_ARG_HEADER = "--maximized";
+    const QString ARG_HEADER = "--";
+    const QString VALUE_HEADER = "=";
+
+    QHash<QString, QString> properties;
+    QString wwwfolder;
+    bool maximized = false;
+
+    QStringList arguments = app.arguments();
+    arguments.pop_front();
+    Q_FOREACH(QString argument, arguments)
+    {
+        if (argument.contains(WWW_LOCATION_ARG_HEADER))
+        {
+            wwwfolder = argument.right(argument.count() - WWW_LOCATION_ARG_HEADER.count());
+        }
+        else
+        if (argument.contains(MAXIMIZED_ARG_HEADER))
+        {
+            maximized = true;
+        }
+        else
+        if (argument.startsWith(ARG_HEADER)
+            && argument.right(argument.count() - ARG_HEADER.count()).contains("="))
+        {
+            QString property = argument.right(argument.count() - ARG_HEADER.count());
+            property = property.left(property.indexOf(VALUE_HEADER));
+            if (property.isEmpty())
+                continue;
+
+            QString value = argument.right(argument.count() - argument.indexOf(VALUE_HEADER) - 1);
+            if (value.isEmpty())
+                continue;
+
+            qDebug() << "Adding property: "
+                     << property
+                     << ", "
+                     << "value: "
+                     << value;
+
+            properties.insert(property, value);
+        }
+        else
+        {
+            qDebug() << "Ignoring argument: " << argument;
+        }
+    }
+
+    if (wwwfolder.isEmpty())
+    {
+        qCritical() << "No (or empty) WWW folder path specified";
+        return EXIT_FAILURE;
+    }
+
+    QFileInfo f(wwwfolder);
+    if (f.isRelative())
+    {
+        f.makeAbsolute();
+    }
+    if (!f.exists() || !f.isDir())
+    {
+        qCritical() << "WWW folder not found or not a proper directory: " << wwwfolder;
+        return EXIT_FAILURE;
+    }
+
+    setUpQmlImportPathIfNecessary();
+
+    QQmlEngine engine;
+    QQmlContext *context = new QQmlContext(engine.rootContext());
+
+    QQmlComponent component(&engine);
+    component.loadUrl(QUrl::fromLocalFile(Webapp::Config::getContainerMainQmlPath()
+                                          + "/main.qml"));
+
+    QObject *object = component.create(context);
+
+    if (!component.isReady())
+    {
+        qWarning() << component.errorString();
+        return -1;
+    }
+
+    if ( ! object)
+    {
+        qCritical() << "Cannot create object from qml base file";
+        return -1;
+    }
+
+    QQuickWindow* window = qobject_cast<QQuickWindow*>(object);
+
+    object->setProperty("htmlIndexDirectory", wwwfolder);
+
+    QHash<QString, QString>::iterator it;
+    for(it = properties.begin();
+        properties.end() != it;
+        ++it)
+    {
+        const char * const pname = it.key().toStdString().c_str();
+        if (object->property(pname).isValid())
+            object->setProperty(pname, it.value());
+    }
+
+    if (window)
+    {
+        if (maximized)
+            window->showMaximized();
+        else
+            window->show();
+    }
+
+    return app.exec();
+}
+
+
+
--- src/ubuntu-html5-app-launcher/main.qml
+++ src/ubuntu-html5-app-launcher/main.qml
@@ -19,9 +19,8 @@
 import QtQuick 2.0
 import Ubuntu.WebApps 0.1
 
-
 WebAppContainer {
     height: 400
     width: 400
-    htmlIndexDirectory: 'www'
 }
+
