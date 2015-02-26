7612de04912bb3a56f891a15549e5fd1350e141f
Adnane Belmadiaf <daker@ubuntu.com>
2015-02-26 16:00:49 +0000

    Fixed class name

--- 0.1/ambiance/js/core.js
+++ 0.1/ambiance/js/core.js
@@ -209,7 +209,7 @@ var UbuntuUI = (function () {
         init: function () {
             this.__setupTabs(document);
             this.__setupPage(document);
-            if (typeof ActionsBar != 'undefined' && ActionsBar ) {
+            if (typeof ActionBar != 'undefined' && ActionBar ) {
                 this.__setupActionsBar(document);
             }
         },
