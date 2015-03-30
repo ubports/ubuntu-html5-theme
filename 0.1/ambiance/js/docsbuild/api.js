bb9cd22bcd712efd2303e982f28ca260fa4bc889
Daniel Holbach <daniel.holbach@canonical.com>
2015-03-30 17:40:48 +0200

    run ./update-docs

---
+++ 0.1/ambiance/js/docsbuild/api.js
@@ -0,0 +1,29 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "UbuntuUI",
+        "UbuntuUI.Button",
+        "UbuntuUI.Dialog",
+        "UbuntuUI.Header",
+        "UbuntuUI.List",
+        "UbuntuUI.OptionSelector",
+        "UbuntuUI.Page",
+        "UbuntuUI.Pagestack",
+        "UbuntuUI.Popover",
+        "UbuntuUI.Progress",
+        "UbuntuUI.Shape",
+        "UbuntuUI.Tab",
+        "UbuntuUI.Tabs",
+        "UbuntuUI.Toolbar"
+    ],
+    "modules": [
+        "UbuntuUI"
+    ],
+    "allModules": [
+        {
+            "displayName": "UbuntuUI",
+            "name": "UbuntuUI"
+        }
+    ]
+} };
+});
